0b389d4cd42d9a3a9a9dc1bb5423c6336aa46838
Gabriel Klein <gabriel.klein@philips.com>
2022-12-18 18:30:28 -0300

    Created the Grid

--- src/components/ResumeItem/index.js
+++ src/components/ResumeItem/index.js
@@ -1,18 +1,15 @@
 import React from "react";
 import * as C from "./styles";
 
-
-const ResumeItem = ( { title, Icon, value }) => {
+const ResumeItem = ({ title, Icon, value }) => {
   return (
-    <>
-      <C.Container>
-        <C.Header>
-          <C.HeaderTitle>{title}</C.HeaderTitle>
-          <Icon />
-        </C.Header>
-        <C.Total>{value}</C.Total>
-      </C.Container>
-    </>
+    <C.Container>
+      <C.Header>
+        <C.HeaderTitle>{title}</C.HeaderTitle>
+        <Icon />
+      </C.Header>
+      <C.Total>{value}</C.Total>
+    </C.Container>
   );
 };
 
